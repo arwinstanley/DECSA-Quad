4bccf56f863fee3f72fdd5f69dbbb3db2387e9ea
arwinstanley <arwds2@gmail.com>
2017-09-08 12:44:11 -0400

    Add files via upload

---
+++ Quadratic/src/QTest.java
@@ -0,0 +1,9 @@
+
+public class QTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Quadratic/src/QuadEqu.java
@@ -0,0 +1,24 @@
+
+public class QuadEqu {
+	private int a;
+	private int b;
+	private int c;
+	private String rootsDescrip="";
+	private int[] roots = new int[2];
+	
+	public QuadEqu(int x, int y, int z) {
+		a= x;
+	    b=y;
+	    c=z;
+	}
+   public int[] quadEqu() {
+	   if 
+	   
+   }
+   public int discrim() {
+	   int d = math.pow(b, 2)- 4*a*c;
+	   if (d>0)
+		   roots=
+	   
+   }
+}
