eb9139932501f37547148cdf24933047ec392578
arwinstanley <arwds2@gmail.com>
2017-09-11 12:52:21 -0400

    Add files via upload

--- Quadratic/src/QuadEqu.java
+++ Quadratic/src/QuadEqu.java
@@ -1,24 +1,37 @@
-
+import java.lang.*;
 public class QuadEqu {
 	private int a;
 	private int b;
 	private int c;
 	private String rootsDescrip="";
 	private int[] roots = new int[2];
+	private int d;
 	
 	public QuadEqu(int x, int y, int z) {
+		if (x==0)
+			throw new IllegalArgumentException();
 		a= x;
 	    b=y;
 	    c=z;
+	    discrim();
 	}
    public int[] quadEqu() {
-	   if 
+	   int[] output= new int[2];
+	   if (d>0)
+		   
 	   
    }
    public int discrim() {
-	   int d = math.pow(b, 2)- 4*a*c;
+	   d =(int) (Math.pow(b, 2)- 4*a*c);
 	   if (d>0)
-		   roots=
-	   
+		   rootsDescrip="two distinct real roots";
+	   else if(d<0) {
+		   roots = null;
+		   rootsDescrip="two distinct complex roots";
+	   }
+	   else if(d==0)
+		   rootsDescrip="one distinct real roots";
+     return d;
    }
 }
+
